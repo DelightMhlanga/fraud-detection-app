0d34ef282f08659e73edfbe274ca5bf754acddb3
Delight Mhlanga <delightmhlanga82@gmail.com>
2025-10-27 14:01:11 +0200

    Bind Flask to port 8080 for Railway

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-# run.py
-
 from app import create_app
-
 from dotenv import load_dotenv
-load_dotenv()
+import os
 
+load_dotenv()
 app = create_app()
 
-
-
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
